684e83a058669994a159ddadb9730c09dce98eb1
Camila Mendonça dos Santos <camilamjpsantos@gmail.com>
2019-09-03 08:31:09 -0300

    cadastro do hospital funcionandooooooooo e oficial

--- cadastrohospital.php
+++ cadastrohospital.php
@@ -8,29 +8,16 @@
 <?php
 	include('conexao.php');
 ?>
-<form method="post">
-	<label>Nome do Hospital:<input type="text" name="hospital"></label>
-	<label>Nome Fantasia:<input type="text" name="fantasia"></label>
-	<label>Razão Social:<input type="text" name="razao"></label>
-	<label>CNPJ:<input type="number" name="cnpj"></label>
-	<label>Telefone:<input type="number" name="tell"></label>
-	<label>E-mail:<input type="mail" name="email"></label>
-	Estado:<select>
-	<?php
-		$sql = "SELECT * FROM tb_estado";
-		$result = $mysqli->query($sql);
-		while($row = $result->fetch_object()){
-			echo '<option name="'.$row->nm_estado.'">'.$row->nm_estado.'</option>';
-		}
-	?>
-	</select>
-	<label>Cidade:<input type="text" name="cidade"></label>
-	<label>CEP:<input type="number" name="cep"></label>
-	<label>Endereço:<input type="texte" name="endereco"></label>
-	<label>Bairro:<input type="text" name="bairro"></label>
-	<label>Número:<input type="number" name="numero"></label>
-	<label>Crie uma senha:<input type="password" name="senha"></label>
-	<label>Confirmação de Senha: <input type="password" name="confsenha"></label>
+<form method="post" enctype="multipart/form-data">
+	<label>Nome do Hospital: <input type="text" name="hospital"></label><br><br>
+	<label>Nome Fantasia: <input type="text" name="fantasia"></label><br><br>
+	<label>Razão Social: <input type="text" name="razao"></label><br><br>
+	<label>CNPJ: <input type="text" name="cnpj"></label><br><br>
+	<label>Telefone: <input type="text" name="tell"></label><br><br>
+	<label>E-mail: <input type="mail" name="email"></label><br><br>
+	<label>Adicione uma foto do Hospital: </label><input type="file" name="file" ><br><br>
+	<label>Crie uma senha: <input type="password" name="senha"></label><br><br>
+	<label>Confirmação de Senha: <input type="password" name="confsenha"></label><br><br>
 	<input type="submit" value="Cadastrar">
 </form>
 <?php  
@@ -43,25 +30,34 @@
 		$cnpj = $_POST['cnpj'];
 		$tell = $_POST['tell'];
 		$email = $_POST['email'];
-		$cidade = $_POST['cidade'];
-		$cep = $_POST['cep'];
-		$endereco = $_POST['endereco'];
-		$bairro = $_POST['bairro'];
-		$numero = $_POST['numero'];
 		$senha = $_POST['senha'];
 		$confsenha = $_POST['confsenha'];
 
-		echo $hora;
-		$sql = "INSERT INTO tb_hospital VALUES (null , '$hospital', '$fantasia', '$cnpj', '$tell', 'foto', '$email', '$senha', '$hora')";
-		$result = $mysqli->query($sql);
-		
-		$sql = "SELECT * FROM tb_hospital";
-		if ($result = $mysqli->query($sql)) {
-			while ($row = $result->fetch_object()) {
-				echo $row->nm_hospital;
+		if ($senha != $confsenha) {		
+			echo "<script>alert('Senhas devem ser iguais !');</script>";
+		}else{
+			if (isset($_FILES['file'])) {	
+				date_default_timezone_set("Brazil/East");
+				$ext = strtolower(substr($_FILES['file']['name'],-4));
+				if ($ext = ".jpg" or ".png" or ".bnp") {
+					$new_name = date("Y.m.d-H.i.s").$ext;
+					$dir = 'foto/';
+					$foto = $dir.$new_name;
+					move_uploaded_file($_FILES['file']['tmp_name'], $foto);
+				}else{
+					echo "Extenção Invalida";
+				}
+			//echo $hora;
+			$sql = "INSERT INTO tb_hospital VALUES (null , '$hospital', '$fantasia', '$cnpj', '$tell', '$foto', '$email', '$senha', '$hora')";
+			$result = $mysqli->query($sql);
 			}
 		}
-	
+
+		//$sql = "SELECT * FROM tb_hospital WHERE ds_data_registro = $hora";
+		//$result = $mysqli->query($sql);
+		//while ($row = $result->fetch_assoc()) {
+			//echo $row->nm_hospital;
+		//}
 	}
 ?>
 </body>
